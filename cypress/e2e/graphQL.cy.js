222e534f5214320b51b9dbaeddb698a85040a6d5
Joan Filipe <joanfilipe0@gmail.com>
2024-01-12 15:04:21 -0300

    Colocando todas Query em uma Fixture

--- cypress/e2e/graphQL.cy.js
+++ cypress/e2e/graphQL.cy.js
@@ -2,91 +2,77 @@ describe('Testando Requisição GraphQL', () => {
 
   it('Testando Query', () => {
 
-    const countryName = `
-      query countryName {
-        countries {
-          name,
-          code,
-          capital
+    cy.fixture('Query').then((fixtureData) => {
+
+      const countryName = fixtureData.testFixtures
+
+      cy.request({
+        method: 'POST',
+        url: 'https://countries.trevorblades.com/',
+        body: {
+          query: countryName
         }
-      }`
-    cy.request({
-      method: 'POST',
-      url: 'https://countries.trevorblades.com/',
-      body: {
-        query: countryName
-      }
-    }).then(response => {
-      expect(response.body.data.countries[0].name).to.be.equal("Andorra")
-    })
+      }).then(response => {
+        expect(response.body.data.countries[0].name).to.be.equal("Andorra")
+      });
+    });
 
   })
 
   it('Obter Informações de um País Específico', () => {
-    const query = `
-      query {
-        country(code: "US") {
-          name
-          capital
-        }
-      }`;
+    cy.fixture('Query').then((fixtureData) => {
+      const query = fixtureData.codeQuery;
 
-    cy.request({
-      method: 'POST',
-      url: 'https://countries.trevorblades.com/',
-      body: { query }
-    }).then(response => {
-      const country = response.body.data.country;
-      expect(country.name).to.be.equal("United States")
-      expect(country.capital).to.be.equal("Washington D.C.")
+      cy.request({
+        method: 'POST',
+        url: 'https://countries.trevorblades.com/',
+        body: { query }
+      }).then(response => {
+        const country = response.body.data.country;
+        expect(country.name).to.equal("United States");
+        expect(country.capital).to.equal("Washington D.C.");
+      });
     });
   });
 
   it('Listar Idiomas de Todos os Países', () => {
-    const query = `
-      query {
-        countries {
-          name
-          languages {
-            name
-          }
-        }
-      }`;
+    cy.fixture('Query').then((fixtureData) => {
+      const query = fixtureData.languagesQuery;
 
-    cy.request({
-      method: 'POST',
-      url: 'https://countries.trevorblades.com/',
-      body: { query }
-    }).then(response => {
-      const countries = response.body.data.countries;
-      countries.forEach(country => {
-        // Antartica não possui linguagem, então seu array pode estar vazio
-        if( country.name = "Antarctica"){
-          expect(country.languages).to.be.an('array')
-        }else
-          expect(country.languages).to.be.an('array').that.is.not.empty;
+      cy.request({
+        method: 'POST',
+        url: 'https://countries.trevorblades.com/',
+        body: { query }
+      }).then(response => {
+        const countries = response.body.data.countries;
+        countries.forEach(country => {
+          // Antarctica não possui linguagem, então seu array pode estar vazio
+          if (country.name === "Antarctica") {
+            expect(country.languages).to.be.an('array');
+          } else {
+            expect(country.languages).to.be.an('array').that.is.not.empty;
+          }
+        });
       });
     });
   });
 
   it('Pesquisar Países por Nome', () => {
-    //Ao usar { name: { eq: "Brazil" } }, você está fornecendo corretamente um objeto StringQueryOperatorInput para o filtro.
-    const query = `
-      query { 
-        countries(filter: { name: { eq: "Brazil" } }) {
-          name
-          capital
-        }
-      }`;
 
-    cy.request({
-      method: 'POST',
-      url: 'https://countries.trevorblades.com/',
-      body: { query }
-    }).then(response => {
-      const country = response.body.data.countries[0];
-      expect(country.name).to.be.equal("Brazil");
+    cy.fixture('Query').then((fixtureData) => {
+      //Ao usar { name: { eq: "Brazil" } }, você está fornecendo corretamente um objeto StringQueryOperatorInput para o filtro.
+      
+      const query = fixtureData.countryQuery
+
+      cy.request({
+        method: 'POST',
+        url: 'https://countries.trevorblades.com/',
+        body: { query }
+      }).then(response => {
+        const country = response.body.data.countries[0];
+        expect(country.name).to.be.equal("Brazil");
+      });
     });
-  });
+  })
 
 })
